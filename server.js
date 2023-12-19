e53ada2f40813a9ba4f15fc9016ca488ded7b366
GuiBoing <guiliboing@gmail.com>
2023-12-19 12:13:33 -0300

    frontend e backend | adicionado angular com pacote tailwind, movido server para dentro do projeto e adicionado pacotes para watch no server

--- server.js
+++ server.js
@@ -5,22 +5,40 @@ const env = require('./env.js');
 const app = express();
 app.use(express.json());
 
-const client = new MongoClient(env.mongoURI);
-var database, collectionCores;
+const client = new MongoClient(env.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+let database, collectionCores;
 
-app.listen(env.port, () => {
-    console.log("DataBird started on port", env.port);
+client.connect()
+  .then(() => {
     database = client.db('passaros');
     collectionCores = database.collection('cores');
-});
+    
+    app.listen(env.port, () => {
+      console.log("DataBird started on port", env.port);
+    });
+  })
+  .catch(err => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
-app.post('/cores', (req, res) => {
-    console.log('POST', 'Cores');
-    collectionCores.insertOne({ nome: req.body.nome, valor: req.body.valor });
-    res.send(`criado cor ${req.body.nome}, valor hex ${req.body.valor}`);
+app.post('/cores', async (req, res) => {
+  console.log('POST', 'Cores');
+  
+  try {
+    await collectionCores.insertOne({ nome: req.body.nome, valor: req.body.valor });
+    res.status(200).json({ code: 200, message: "Operação concluída com sucesso" });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ code: 500, message: "Erro interno no servidor" });
+  }
 });
 
 app.get('/cores', async (req, res) => {
+  try {
     let results = await collectionCores.find({}).toArray();
-    res.send(results).status(200);
+    res.status(200).json({ code: 200, message: "Operação concluída com sucesso", data: results });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ code: 500, message: "Erro interno no servidor" });
+  }
 });
---
+++ src/app/cores/cores.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cores',
+  standalone: true,
+  imports: [],
+  templateUrl: './cores.component.html',
+  styleUrl: './cores.component.scss'
+})
+export class CoresComponent {
+
+}
